285a9a030bfeb4494bf6787162c058add1d0edf7
dheeraj <dheerajrvithalkar@gmail.com>
2024-07-29 18:06:16 +0530

    while and do while loops done

---
+++ doWHile.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i = 6;
+    do
+    {
+        cout << "Dheeraj" << i << endl;
+        i++;
+    } while (i <= 8);
+}
+
+// the value in while condition needs to be a upper limit and the other value in do condition is the start point or 
