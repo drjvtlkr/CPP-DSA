bb4ebfc37dbed875d98380b1c5516e1a7d758731
dheeraj <dheerajrvithalkar@gmail.com>
2024-07-29 17:51:26 +0530

    strings and forloop

---
+++ ForLoops.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int times;
+    cin >> times;
+    for (int i=1; i<= times; i++){
+        cout << "Dheeraj"<< endl;
+    }
+}
---
+++ Strings.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "Dheeraj";
+    int len = s.size();
+    cout << s[len-1]<< endl;
+    int f = len-1;
+    cout<< s[4]<< len<< f;
+}
