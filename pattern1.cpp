d5fd148fa2516ac20f708c9aa6f24ba73ff07a48
dheeraj <dheerajrvithalkar@gmail.com>
2024-08-02 18:44:24 +0530

    pattern with while and recussion

--- pattern1.cpp
+++ pattern1.cpp
@@ -8,9 +8,49 @@ void print(int n)
     {
         for (int j = 0; j < n; j++)
         {
-            cout << "*";
+            cout << " * ";
         }
         cout << endl;
+    }
+}
+
+void pattern(int n)
+{
+    int i = 0, j = 0;
+    while (i < n)
+    {
+        while (j < n)
+        {
+            cout << "* ";
+            j++;
+        }
+        j = 0;
+        i++;
+        cout << endl;
+    }
+}
+
+void recur(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    cout << "  * ";
+    recur(n-1);
+}
+
+void recursion(int n, int nn)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    else
+    {
+        recur(nn);
+        cout << endl;
+        recursion(n-1, nn);
     }
 }
 
@@ -19,4 +59,8 @@ int main()
     int n;
     cin >> n;
     print(n);
+    pattern(n);
+    cout << n<< endl;
+    recursion(n, n);
+    
 }
