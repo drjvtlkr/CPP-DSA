432b2315428cadfaa8e497dcf0e1adbeb9d6f825
dheeraj <dheerajrvithalkar@gmail.com>
2024-08-07 00:22:24 +0530

    pattern 3 and 4 done

--- pattern4.cpp
+++ pattern4.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-void logic(int n)
+void forLoop(int n)
 {
+    int k = 2 * n - 2;
+
     for (int i = n; i > 0; i--)
     {
+        for (int j = 0; j < n - i; j++)
+            cout << "  ";
+        k = k - 2;
         for (int j = 0; j < i; j++)
         {
             cout << "* ";
@@ -14,11 +18,17 @@ void logic(int n)
     }
 }
 
-void logic2(int n)
+void whileLoop(int n)
 {
-    int i = n, j = 0;
+    int i = n, j = 0, k = 0;
     while (i > 0)
     {
+        while (k < (n - i))
+        {
+            cout << "  ";
+            k++;
+        }
+        k = 0;
         while (j < i)
         {
             cout << "* ";
@@ -30,33 +40,11 @@ void logic2(int n)
     }
 }
 
-void recur(int n)
-{
-    if (n == 0)
-    {
-        return;
-    }
-    cout << "* ";
-    recur(n - 1);
-}
-
-void recursion(int n)
-{
-    if (n == 0)
-    {
-        return;
-    }
-    recur(n);
-    cout << endl;
-    recursion(n - 1);
-}
-
 int main()
 {
     int n;
     cin >> n;
-    logic(n);
-    logic2(n);
-    recursion(n);
+    forLoop(n);
+    whileLoop(n);
     return 0;
 }
