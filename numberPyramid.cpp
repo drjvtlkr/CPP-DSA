849e9de8928c7912a7f88b682820a84bc4f82731
dheeraj <dheerajrvithalkar@gmail.com>
2024-08-09 22:18:46 +0530

    number patterns

---
+++ numberPyramid.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void loop(int n){
+    int num=1;
+
+for (int i = 0; i < n; i++)
+    {
+        for (int j = 0 ; j <= i; j++)
+        {
+            cout << num<<" ";
+        }
+        num= num+1;
+        cout << endl;
+    }
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    loop(n);
+    return 0;
+}
